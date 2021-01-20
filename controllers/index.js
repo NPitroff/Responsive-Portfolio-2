0bc14e644945f407b48961a0a3f5ba40163142d8
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-20 14:38:08 -0800

    Added data in the different handlebars to diplay data

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
+const bio = require("./bio");
+const contact = require("./contact");
 
 router.use('/', homeRoutes);
 
